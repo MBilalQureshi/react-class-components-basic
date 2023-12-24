187d521b407666e6800efd844f0b4b3dc5cfce91
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-12-24 17:46:17 +0000

    Add basic click button event that change button and text

--- my-app/src/component/StatefulGreeting.js
+++ my-app/src/component/StatefulGreeting.js
@@ -12,13 +12,21 @@ class StatefulGreeting extends React.Component {
         };
     }
 
+    handleClick(){
+        // Updating the state of introduction that was initial set in constructor
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText : "Enter",
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>
                     {this.state.introduction} {this.props.greeting}
                 </h1>
-                <button>
+                <button onClick={() => this.handleClick()}>
                     {this.state.buttonText}
                 </button>
             </div>
