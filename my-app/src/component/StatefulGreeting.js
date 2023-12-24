de0ac24e461c99c6b7da1e11cc6efe4ee789fda5
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-12-24 13:35:44 +0000

    Add constructor

--- my-app/src/component/StatefulGreeting.js
+++ my-app/src/component/StatefulGreeting.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class StatefulGreeting extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            // initial state values
+        };
+    }
+
     render() {
         return <h1>Hello, {this.props.greeting} my name is {this.props.name}</h1>
     }
